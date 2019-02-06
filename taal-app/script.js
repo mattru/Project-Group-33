a6f9bef1848d84afeb9e720e391aa4888578540c
SirNexus <aidan.kodi@gmail.com>
2019-02-05 16:51:28 -0800

    Clean up script.js, additional shapes functionality
    Clean up code and remove most console.log statements. Add
    functionality to clear last shape as soon as the button to add a
    new shape is clicked.

--- taal-app/script.js
+++ taal-app/script.js
@@ -1,55 +1,51 @@
-console.log("In script.js")
-
 var map;
 var drawingManager;
 var selectedShape = null;
+var prevShape = null;
 
-document.getElementById("map").addEventListener("", function(event) {
-    alert(event);
+document.getElementById("rectangle-button").addEventListener("click", function() {
+
+    clearPrev();
+
+    drawingManager.setOptions({
+        drawingMode : google.maps.drawing.OverlayType.RECTANGLE,
+        drawingControl : true,
+        drawingControlOptions : {
+            position : google.maps.ControlPosition.TOP_CENTER,
+            drawingModes : [ google.maps.drawing.OverlayType.RECTANGLE ]
+        },
+
+    });
+
+    drawingManager.setMap(map);
 });
 
-document.getElementById("rectangle_button").addEventListener("click", function(event) {
-    //Setting options for the Drawing Tool. In our case, enabling Polygon shape.
-		drawingManager.setOptions({
-			drawingMode : google.maps.drawing.OverlayType.RECTANGLE,
-			drawingControl : true,
-			drawingControlOptions : {
-				position : google.maps.ControlPosition.TOP_CENTER,
-				drawingModes : [ google.maps.drawing.OverlayType.RECTANGLE ]
-			},
-			rectangleOptions : {
-				strokeColor : '#6c6c6c',
-				strokeWeight : 3.5,
-				fillColor : '#926239',
-				fillOpacity : 0.6,
-                editable: true,
-              draggable: true
-            }
-		});
-		// Loading the drawing Tool in the Map.
-		drawingManager.setMap(map);
+document.getElementById("circle-button").addEventListener("click", function() {
+
+    clearPrev();
+
+    drawingManager.setOptions({
+        drawingMode : google.maps.drawing.OverlayType.CIRCLE,
+        drawingControl : true,
+        drawingControlOptions : {
+            position : google.maps.ControlPosition.TOP_CENTER,
+            drawingModes : [ google.maps.drawing.OverlayType.CIRCLE ]
+        }
+    });
+
+    drawingManager.setMap(map);
 });
 
-document.getElementById("circle_button").addEventListener("click", function(event) {
-    //Setting options for the Drawing Tool. In our case, enabling Polygon shape.
-		drawingManager.setOptions({
-			drawingMode : google.maps.drawing.OverlayType.CIRCLE,
-			drawingControl : true,
-			drawingControlOptions : {
-				position : google.maps.ControlPosition.TOP_CENTER,
-				drawingModes : [ google.maps.drawing.OverlayType.CIRCLE ]
-			},
-			circleOptions : {
-				strokeColor : '#6c6c6c',
-				strokeWeight : 3.5,
-				fillColor : '#926239',
-				fillOpacity : 0.6,
-                editable: true,
-              draggable: true
-            }
-		});
-		// Loading the drawing Tool in the Map.
-		drawingManager.setMap(map);
+document.getElementById("generate-flight").addEventListener("click", function () {
+    if (prevShape) {
+        let bounds = selectedShape.getBounds().getNorthEast();
+
+        new google.maps.Marker({position:bounds, map:map});
+        new google.maps.Marker({position:selectedShape.getBounds().getSouthWest(), map:map})
+
+        console.log(bounds);
+    }
+    
 });
 
 function initMap() {
@@ -60,27 +56,42 @@ function initMap() {
 
     drawingManager = new google.maps.drawing.DrawingManager();
 
-    google.maps.event.addListener(drawingManager, 'overlaycomplete', completeRectangle);
-
+    google.maps.event.addListener(drawingManager, 'overlaycomplete', completePolygon);
     google.maps.event.addListener(map, 'click', clearSelection);
+
+    drawingManager.setOptions({
+        drawingMode : google.maps.drawing.OverlayType.RECTANGLE,
+        drawingControl : true,
+        drawingControlOptions : {
+            position : google.maps.ControlPosition.TOP_CENTER,
+            drawingModes : [ google.maps.drawing.OverlayType.RECTANGLE ]
+        },
+        polygonOptions : {
+            strokeColor : '#6c6c6c',
+            strokeWeight : 3.5,
+            fillColor : '#926239',
+            fillOpacity : 0.6,
+            editable: true,
+            draggable: true
+        }
+    });
 
 };
 
-function completeRectangle(event) {
-    console.log(event);
-
+function completePolygon(event) {
     let newShape = event.overlay;
-
-    google.maps.event.addListener(newShape, 'click', function() {
-        setSelection(newShape);
-      });
 
     drawingManager.setDrawingMode(null);
     drawingManager.setOptions({
         drawingControl: false
     });
 
+    google.maps.event.addListener(newShape, 'click', function() {
+        setSelection(newShape);
+    });
+
     selectedShape = newShape;
+    prevShape = newShape;
 };
 
 function setSelection(newShape) {
@@ -98,6 +109,12 @@ function clearSelection() {
         selectedShape = null;
     }
 
+}
+
+function clearPrev() {
+    if (prevShape) {
+        prevShape.setMap(null);
+    }
 }
 
 
